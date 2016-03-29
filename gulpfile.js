3fe76cf84a936495da6d5aad052f19779d29e467
lrfung <luorongfang126@126.com>
2016-03-29 09:35:41 +0800

    修改

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var gulp = require('gulp'), //本地安装gulp所用到的地方
 gulp.task('styles', function () {
     gulp.src('./src/css/*.less') //该任务针对的文件
       .pipe(less()) //该任务调用的模块       
-      .pipe(gulp.dest('./dist/css/')) //将会在src/css下生成index.css
+      .pipe(gulp.dest('./dist/css/')) //将会在dist/css下生成index.css
       .pipe(concat('main.css'))
       // .pipe(gulp.dest('dist/styles'))
       // .pipe(rename({ suffix: '.min' }))
